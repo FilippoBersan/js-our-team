8fc54569ca6ba34e1f968a285324a0e9bc718d1d
FilippoBersan <filippobersan99@gmail.com>
2023-11-20 15:08:05 +0100

    feat: milestone 1

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+let nameTeam = {
+  'Wayne Barret': 'Founder & CEO',
+  'Angela Caroll': 'Chief Editor',
+  'Walter Gordon': 'Office Manager',
+  'Angela Lopez': 'Social Media Manager',
+  'Scott Estrada': 'Developer',
+  'Barbara Ramos': 'Graphic Designer',
+};
+
+console.log(nameTeam);
