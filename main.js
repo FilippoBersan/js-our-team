a57f22fdc6b343c5f2e05211c038be4cad008b61
FilippoBersan <filippobersan99@gmail.com>
2023-11-20 17:55:25 +0100

    fix: milestone 1

--- main.js
+++ main.js
@@ -9,13 +9,47 @@
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
-let nameTeam = {
-  'Wayne Barret': 'Founder & CEO',
-  'Angela Caroll': 'Chief Editor',
-  'Walter Gordon': 'Office Manager',
-  'Angela Lopez': 'Social Media Manager',
-  'Scott Estrada': 'Developer',
-  'Barbara Ramos': 'Graphic Designer',
-};
+const nameTeam = [
+  {
+    nome: 'Wayne Barret',
+    ruolo: 'Founder & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg',
+  },
+
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'angela-caroll-chief-editor.jpg',
+  },
+
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg',
+  },
+
+  {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'angela-lopez-social-media-manager.jpg',
+  },
+
+  {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg',
+  },
+
+  {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
 
 console.log(nameTeam);
+
+// const nomi = document.createElement('p');
+
+// let paragrafo = (document.getElementById('myid').innerHTML =
+//   nameTeam.toString());
